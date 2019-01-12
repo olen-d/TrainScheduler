d5713910c1881f64f79edfae671691249f577622
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-12 01:02:49 -0500

    Added edit & delete capabilities

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ let trains = [];
 let trainsRef = dataRef.ref("/trains");
 
 const trainSchedules = {
-    addTrain: () => {
+    addTrain() {
         train["name"] = $("#train-name").val().trim();
         train["destination"] = $("#train-destination").val().trim();
         train["first"] = $("#first-train").val().trim();
@@ -29,7 +29,7 @@ const trainSchedules = {
         });
     },
     
-    updateTimes: (first, freq) => {
+    updateTimes(first, freq) {
         let mFirst = moment(first,"HH:mm");
         let mInterval = moment().diff(mFirst,"minutes");
 
@@ -47,7 +47,7 @@ const trainSchedules = {
         return {nextArrival: nextArrival, minutesAway: minutesAway};
     },
 
-    displaySchedule: () => {
+    displaySchedule() {
         trainsRef.orderByChild("name").on("child_added", function(snapshot) {
 
             let sv = snapshot.val();
@@ -61,8 +61,18 @@ const trainSchedules = {
     
             let times = trainSchedules.updateTimes(first, freq);
             
-            let newRow = `<tr id=\"${sk}\" data-first=\"${first}\" data-frequency=\"${freq}\"><th scope=\"row\">${sv.name}</th><td>${sv.destination}</td><td>${freq}</td><td class=\"next-arrival\">${times.nextArrival}</td><td class=\"minutes-away\">${times.minutesAway}</td></tr>`;
+            let newRow = `<tr id=\"${sk}\" data-first=\"${first}\" data-frequency=\"${freq}\"><th scope=\"row\" class=\"name\" >${sv.name}</th><td class=\"dest\">${sv.destination}</td><td class=\"freq\">${freq}</td><td class=\"next-arrival\">${times.nextArrival}</td><td class=\"minutes-away\">${times.minutesAway}</td><td><a href=\"#edit-train\"><i class=\"fas fa-edit edit\" data-id=\"${sk}\"></i></a></td><td><i class=\"fas fa-times delete\" data-id=\"${sk}\" ></i></td></tr>`;
             $("#trains").append(newRow);
+
+            //  Event listener to delete train
+            $(`#${sk} .delete`).on("click", function () {
+                trainSchedules.deleteTrain(this);
+            });
+
+            // Event listener to edit train
+            $(`#${sk} .edit`).on("click", function () {
+                trainSchedules.editTrain(this);
+            });
             
             // Store train keys for the updates
             trains.push(sk);
@@ -73,11 +83,16 @@ const trainSchedules = {
           });
     },
 
-    currentDate: () => {
+    currentDate() {
         $("#current-date").text(moment().format("dddd, MMMM Do, YYYY"));
     },
 
-    updateSchedule: () => {
+    currentTime() {
+        let currTime = moment().format("h:mm a");
+        $("#current-time").text(currTime);
+    },
+
+    updateSchedule() {
         setInterval(() => {
             trains.forEach((v) => {
                 let first = $(`#${v}`).attr("data-first");
@@ -86,9 +101,111 @@ const trainSchedules = {
                 $(`#${v} .next-arrival`).text(times.nextArrival);
                 $(`#${v} .minutes-away`).text(times.minutesAway);
             })
-            
+            trainSchedules.currentTime();
         },60000); 
-    } 
+    },
+    
+    deleteTrain(thisTrain) {
+        let trainKey = ($(thisTrain).attr("data-id"));
+        td = document.getElementById("delete-dialog");
+        td.showModal();
+        $("#delete-submit").on("click", () => {
+            trainsRef.child(trainKey).remove();
+            td.close();
+            });
+        $("#delete-cancel").on("click", () => {
+            td.close();
+            return;
+        });
+    },
+
+    deleteRow() {
+        trainsRef.on("child_removed", function(oldChildSnapshot) {
+            let trainKey = oldChildSnapshot.ref.key;
+            $(`#${trainKey}`).remove();
+        });   
+    },
+
+    editTrain(thisTrain) {
+        let trainKey = ($(thisTrain).attr("data-id"));
+        let trainRef = dataRef.ref(`/trains/${trainKey}`)
+        
+        trainRef.once("value", function(snapshot) {
+            let sv = snapshot.val();
+            $("#train-id").val(trainKey);
+            $("#train-name").val(sv.name);
+            $("#train-destination").val(sv.destination);
+            $("#first-train").val(sv.first);
+            $("#train-frequency").val(sv.frequency);
+            $("#train-added").val(sv.dateAdded);
+
+            // Change Add Train and switch the submit buttons
+            $("#train-form-header").text("Edit a Train");
+            $("#add-train").css("display","none");
+            $("#edit-train").css("display","block");
+
+            
+        // Handle any errors
+        }, (errorObject) => {
+            console.log("Errors handled: " + errorObject.code);
+        });        
+    },
+
+    updateTrain() {
+        // Update the Database
+        let trainKey = $("#train-id").val().trim();
+        let trainRef = dataRef.ref(`/trains/${trainKey}`)
+      
+        train["name"] = $("#train-name").val().trim();
+        train["destination"] = $("#train-destination").val().trim();
+        train["first"] = $("#first-train").val().trim();
+        train["frequency"] = $("#train-frequency").val().trim();
+        train["dateAdded"] = $("#train-added").val().trim();
+        train["dateUpdated"] = firebase.database.ServerValue.TIMESTAMP;
+
+        trainRef.set(train, (error) => {
+            (error ? console.log("Errors handled " + error) : console.log("Train successfully updated in the database. "));
+        });
+
+        // Change Edit Train and switch the submit buttons
+        $("#train-form-header").text("Add a Train");
+        $("#edit-train").css("display","none");
+        $("#add-train").css("display","block");
+
+        // Clean up
+        trainSchedules.clearTrainFields();
+    },
+
+    clearTrainFields() {
+        $("#train-id").val("");
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#first-train").val("");
+        $("#train-frequency").val("");
+    },
+
+    updateRow() {
+        trainsRef.on("child_changed", function(snapshot) {
+            let trainKey = snapshot.ref.key;
+            let sv = snapshot.val();
+            let nextArrival = 0;
+            let minutesAway = 0;
+    
+            let first = sv.first;
+            let freq = sv.frequency;
+    
+            let times = trainSchedules.updateTimes(first, freq);
+
+            $(`#${trainKey}`).attr("data-first", first);
+            $(`#${trainKey}`).attr("data-frequency", freq);
+
+            $(`#${trainKey} .name`).text(sv.name);
+            $(`#${trainKey} .dest`).text(sv.destination);
+            $(`#${trainKey} .freq`).text(sv.frequency);
+            $(`#${trainKey} .next-arrival`).text(times.nextArrival);
+            $(`#${trainKey} .minutes-away`).text(times.minutesAway);
+        });   
+    }
 }
 
 $(document).ready(() => {
@@ -97,7 +214,15 @@ $(document).ready(() => {
         trainSchedules.addTrain();
     });
 
+    $("#edit-train").on("click", (e) => {
+        e.preventDefault();
+        trainSchedules.updateTrain();
+    });
+
+    trainSchedules.deleteRow();
+    trainSchedules.updateRow();
     trainSchedules.displaySchedule();
     trainSchedules.currentDate();
+    trainSchedules.currentTime();
     trainSchedules.updateSchedule();
 });
